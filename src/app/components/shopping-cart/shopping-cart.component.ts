96fbbd39f0217734e0c1488ca36d6d6fc0dd9efb
karol <carolina.chamorro@securitasdirect.es>
2019-10-24 17:08:00 +0200

    feat: add discount drinks

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -13,18 +13,40 @@ export class ShoppingCartComponent implements OnInit {
   private shoppingCart: Array<Food> = [];
   private subscription: Subscription;
   private total: number;
-  
+  private discount = 0;
   constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
     this.shoppingCartService.getShoppingCart().subscribe(data => {
-      console.log(data);
+      const getTotal = this.shoppingCartService.getTotal();
+
       this.shoppingCart = data;
-      this.total = this.shoppingCartService.getTotal();
+      this.discount = this.getDiscount(data, getTotal);
+      const calcTotal = getTotal - this.discount;
+
+      this.total = this.trunc(calcTotal, 2);
     },
       error => alert(error));
   }
 
+  trunc(x, position = 0) {
+    const s = x.toString();
+    const decimalLength = s.indexOf('.') + 1;
+    const numStr = s.substr(0, decimalLength + position);
+    return Number(numStr);
+  }
+
+  getDiscount(data, getTotal) {
+    const discountDrinks = data.reduce((ac, el) => {
+        ac[el.category] = (ac[el.category] || 0) + 1;
+        return ac;
+    }, {});
+
+    if (discountDrinks && discountDrinks.Drinks >= 10) {
+      return this.trunc(getTotal * 0.1, 2);
+    }
+    return 0;
+  }
   removeProduct(food, i) {
     this.shoppingCartService.removeShoppingCart(food, i);
   }
